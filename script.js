172731761bb90aec04d0e6ee077aac4960fbc305
nvonil <itslinonavarro@gmail.com>
2025-07-13 21:25:47 -0700

    Added basic JS to test render functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Test");
+leadList = ["this", "is", "a", "test!"];
+
+const input = document.querySelector(".input");
+
+const addButton = document.querySelector(".add-button");
+const tabButton = document.querySelector(".tab-button");
+const deleteButton = document.querySelector(".delete-button");
+
+function render() {
+    for (let i = 0; i < leadList.length; i++) {
+        console.log(leadList[i]);
+    }
+}
+
+render();
