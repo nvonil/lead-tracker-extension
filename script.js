bf8fa98b1ee3124a912baff9be70783e4e8f4d9d
nvonil <itslinonavarro@gmail.com>
2025-07-14 21:22:53 -0700

    Added logic for add and clear buttons, added render function

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-leadList = ["this", "is", "a", "test!"];
+leadsList = [];
 
-const input = document.querySelector(".input");
+const leadInput = document.querySelector(".lead-input");
 
 const addButton = document.querySelector(".add-button");
-const tabButton = document.querySelector(".tab-button");
-const deleteButton = document.querySelector(".delete-button");
+const saveButton = document.querySelector(".save-button");
+const clearButton = document.querySelector(".clear-button");
+
+const divider = document.querySelector(".divider");
+
+const leadsContainer = document.querySelector(".leads-container");
+
+addButton.addEventListener("click", function () {
+    if (leadInput.value.trim() === "") {
+        alert("Please enter a lead.");
+    } else {
+        leadsList.push(leadInput.value);
+        leadInput.value = "";
+
+        divider.style.display = "block";
+        leadsContainer.style.display = "flex";
+        render();
+    }
+});
+
+clearButton.addEventListener("dblclick", function () {
+    if (leadsList.length === 0) {
+        alert("No leads to delete.");
+    } else {
+        leadsList = [];
+        render();
+    }
+});
 
 function render() {
-    for (let i = 0; i < leadList.length; i++) {
-        console.log(leadList[i]);
+    renderedLeads = "";
+    for (let i = 0; i < leadsList.length; i++) {
+        renderedLeads += `<div class="lead-item">
+                            <a href="${leadsList[i]}">${leadsList[i]}</a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-x-icon lucide-x"
+                                >
+                                <path d="M18 6 6 18" />
+                                <path d="m6 6 12 12" />
+                            </svg>
+                         </div>`;
     }
+    leadsContainer.innerHTML = renderedLeads;
 }
-
-render();
